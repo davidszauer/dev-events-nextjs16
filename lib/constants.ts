0b31ba310de06e316e2f323dd2bf7d3d2e8912df
David <david.szauer@gmail.com>
2025-12-11 01:54:37 +0100

    Creating Homepage

---
+++ lib/constants.ts
@@ -0,0 +1,51 @@
+export const events = [
+  {
+    image: '/images/event1.png',
+    title: 'React Summit 2024',
+    slug: 'react-summit-2024',
+    location: 'Amsterdam, Netherlands',
+    date: '2024-06-14',
+    time: '09:00 AM'
+  },
+  {
+    image: '/images/event2.png',
+    title: 'Next.js Conf 2024',
+    slug: 'nextjs-conf-2024',
+    location: 'San Francisco, CA',
+    date: '2024-10-22',
+    time: '10:00 AM'
+  },
+  {
+    image: '/images/event3.png',
+    title: 'DevOps World 2024',
+    slug: 'devops-world-2024',
+    location: 'Orlando, FL',
+    date: '2024-09-18',
+    time: '08:30 AM'
+  },
+  {
+    image: '/images/event4.png',
+    title: 'AI Hackathon 2024',
+    slug: 'ai-hackathon-2024',
+    location: 'New York, NY',
+    date: '2024-08-10',
+    time: '12:00 PM'
+  },
+  {
+    image: '/images/event5.png',
+    title: 'Web3 Developers Meetup',
+    slug: 'web3-developers-meetup',
+    location: 'Austin, TX',
+    date: '2024-07-25',
+    time: '06:00 PM'
+  },
+  {
+    image: '/images/event6.png',
+    title: 'TypeScript Conference',
+    slug: 'typescript-conference',
+    location: 'London, UK',
+    date: '2024-11-05',
+    time: '09:30 AM'
+  }
+];
+
