17b52068792f9750f4535c7b8aeb359ca814d4bc
David <david.szauer@gmail.com>
2025-12-15 01:29:16 +0100

    Implement database models

---
+++ database/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Database models export file
+ * Central export point for all Mongoose models
+ * 
+ * Usage:
+ *   import { Event, Booking } from '@/database';
+ */
+
+export { Event, type IEvent } from './event.model';
+export { Booking, type IBooking } from './booking.model';
